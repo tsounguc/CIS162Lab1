48f32d49dc2ec3c363f2859dca5391631ada04af
tsounguc <tsounguc@mail.gvus.edu>
2020-01-11 03:52:34 -0500

    WINTER 2018: Lab1

---
+++ Art.java
@@ -0,0 +1,30 @@
+
+/**
+ * Write a description of class Art here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Art
+{
+    public static void main (String [] args)
+    {
+        System.out.println("your art goes here");
+        System.out.println("   .-\"\"\"\"-.        .-\"\"\"\"-.");
+        System.out.println("  /        \\      /        \\     ");
+        System.out.println(" /_        _\\    /_        _\\    ");
+        System.out.println("// \\      / \\\\  // \\      / \\\\");
+        System.out.println("|\\__\\    /__/|  |\\__\\    /__/|");
+        System.out.println(" \\    ||    /    \\    ||    /   ");
+        System.out.println("  \\        /      \\        /    ");
+        System.out.println("   \\  __  /        \\  __  /     ");
+        System.out.println("    '.__.'          '.__.'        ");
+        System.out.println("     |  |            |  |        ");
+        System.out.println("jgs  |  |            |  |        ");
+
+
+        
+
+        
+    }
+}
